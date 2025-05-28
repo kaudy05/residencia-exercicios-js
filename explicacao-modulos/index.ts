0ba47defefe4d58b00b208d598b800c86b020461
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2025-05-28 15:48:39 -0300

    feat(modules): explicação de módulos adicionada

---
+++ explicacao-modulos/index.ts
@@ -0,0 +1,7 @@
+import { soma, div, mult } from "./operacoes/matematica";
+import subtracao from "./operacoes/matematica";
+
+console.log(soma(10, 20));
+console.log(subtracao(10, 20));
+console.log(div(10, 20));
+console.log(mult(10, 20));
