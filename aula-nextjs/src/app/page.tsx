8cf631006a049a6e8ea1829d1606d79cb96d7c92
kaudy5 <kauysantosferreira109@gmail.com>
2025-05-30 17:45:01 -0300

    feat/naruto tema 4

--- aula-nextjs/src/app/page.tsx
+++ aula-nextjs/src/app/page.tsx
@@ -1,6 +1,30 @@
 "use client";
+import React, { useEffect, useState } from "react";
+
+
 import styles from "./page.module.css";
 
-export default function Home() {
-  return <button className={styles.page}></button>;
+//export default function Home() { 
+
+function app() {
+  const [texto, setText] = useState('');
+
+  useEffect(() => {
+    if (texto.toLowerCase() === 'naruto'){
+    alert('Você digitou naruto');
+  }
+
+}, [texto]);
+return (
+  
+  <button className={styles.page}>
+    <input
+    type="text"
+    value={texto}
+    onChange={(e) => setText(e.target.value)}
+    placeholder="Digite algo..."
+    ></input>
+</button>
+);
 }
+export default app;
